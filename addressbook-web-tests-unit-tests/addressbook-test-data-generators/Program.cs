ecd42bd841fbf3d62189d7f0b96770b4dedd1970
shkodina <blobby@yandex.ru>
2020-09-04 17:17:10 +0300

    - генератор тестовых данных работает с excel

--- addressbook-web-tests-unit-tests/addressbook-test-data-generators/Program.cs
+++ addressbook-web-tests-unit-tests/addressbook-test-data-generators/Program.cs
@@ -8,6 +8,7 @@ using addressbook_web_tests_unit_tests;
 using System.Xml;
 using System.Xml.Serialization;
 using Newtonsoft.Json;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace addressbook_test_data_generators
 {
@@ -15,29 +16,36 @@ namespace addressbook_test_data_generators
     {
         static void Main(string[] args)
         {
-            StreamWriter writer = new StreamWriter(args[1]);
             //string format = args[2];
-            string format = args[1].Split('.').Last();
+            int countOfGroupsToCreate = Convert.ToInt32(args[0]);
+            string fileName = args[1];
+            string format = fileName.Split('.').Last();
 
-            List<GroupData> groups = new List<GroupData>(GroupsTests.RandomGroupProvider(Convert.ToInt32(args[0])));
-
-            switch (format)
+            List<GroupData> groups = new List<GroupData>(GroupsTests.RandomGroupProvider(countOfGroupsToCreate));
+            if (format == "xlsx")
             {
-                case "csv":
-                    WriteGroupsToCSVFile(groups, writer);
-                    break;
-                case "xml":
-                    WriteGroupsToXMLFile(groups, writer);
-                    break;
-               case "json":
-                    WriteGroupsToJSONFile(groups, writer);
-                    break;
-                default:
-                    System.Console.Out.WriteLine("Unknown format: " + format);
-                    break;
+                WriteGroupsToExcelFile(groups, fileName);
             }
-            
-            writer.Close();
+            else
+            {
+                StreamWriter writer = new StreamWriter(fileName);
+                switch (format)
+                {
+                    case "csv":
+                        WriteGroupsToCSVFile(groups, writer);
+                        break;
+                    case "xml":
+                        WriteGroupsToXMLFile(groups, writer);
+                        break;
+                    case "json":
+                        WriteGroupsToJSONFile(groups, writer);
+                        break;
+                    default:
+                        System.Console.Out.WriteLine("Unknown format: " + format);
+                        break;
+                }
+                writer.Close();
+            }
         }
 
         static void WriteGroupsToCSVFile(List<GroupData> groups, StreamWriter writer)
@@ -55,6 +63,29 @@ namespace addressbook_test_data_generators
        static void WriteGroupsToJSONFile(List<GroupData> groups, StreamWriter writer) 
         {
             writer.Write(JsonConvert.SerializeObject(groups, Newtonsoft.Json.Formatting.Indented));
+        }
+       static void WriteGroupsToExcelFile(List<GroupData> groups, string fileName) 
+       {
+            Excel.Application app = new Excel.Application();
+            app.Visible = true;
+            Excel.Workbook wb = app.Workbooks.Add();
+            Excel.Worksheet sheet = wb.ActiveSheet;
+
+            int row = 1;
+            foreach (GroupData gr in groups)
+            {
+                sheet.Cells[row, 1] = gr.Name;
+                sheet.Cells[row, 2] = gr.Header;
+                sheet.Cells[row, 3] = gr.Footer;
+                row++;
+            }
+
+            string fullPath = Path.Combine(new BaseData().TestDataBaseAddress, fileName);
+            File.Delete(fullPath);
+            wb.SaveAs(fullPath);
+
+            wb.Close();
+            app.Visible = false;
         }
     }
 }
--- addressbook-web-tests-unit-tests/addressbook-web-tests-unit-tests/model/BaseData.cs
+++ addressbook-web-tests-unit-tests/addressbook-web-tests-unit-tests/model/BaseData.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace addressbook_web_tests_unit_tests
 {
-    class BaseData
+    public class BaseData
     {
         private string baseURL = "http://localhost/addressbook/";
         private string testDataBaseAddress = @"C:\Users\user\Source\Repos\csharp_training\addressbook-web-tests-unit-tests\addressbook-test-data-generators\bin\Debug\";
